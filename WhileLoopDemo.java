d2c6d96ef67b21ef8f035d2577070e4a530ceb24
skull-crusher-aze <87946333+skull-crusher-aze@users.noreply.github.com>
2024-03-02 01:51:11 +0400

    Create WhileLoopDemo.java

---
+++ WhileLoopDemo.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //While
+        int i = 0;
+        while(i<10){
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("SYSTEM PAUZA");
+
+    }
+}
