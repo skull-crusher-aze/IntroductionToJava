c5d87225f0211efbd899e41e7109ff55af3c1cb9
skull-crusher-aze <87946333+skull-crusher-aze@users.noreply.github.com>
2024-03-02 01:42:52 +0400

    Create SwitchCase.java

---
+++ SwitchCase.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'C';
+        switch (grade) {
+            case 'A':
+                System.out.println("Kecdin");
+                break;
+            case 'B':
+                System.out.println("Pis deyil");
+                break;
+            case 'C':
+                System.out.println("Gucnen Belaynan kectin");
+                break;
+            default:
+                System.out.println("Kesildin");
+        }
+
+    }
+}
