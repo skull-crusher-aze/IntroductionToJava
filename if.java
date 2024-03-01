829a6730f2e68f75e8f13fdc8cde655a5eb30a3b
skull-crusher-aze <87946333+skull-crusher-aze@users.noreply.github.com>
2024-03-02 01:22:57 +0400

    Create if.java

---
+++ if.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int say = 20;
+        if(say < 20){
+            System.out.println(say);
+        }
+        else if(say > 20){
+            System.out.println(say+2);
+        }
+        else{
+            System.out.println(say+3);
+        }
+    }
+}
