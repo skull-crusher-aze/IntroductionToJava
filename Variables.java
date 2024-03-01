cff06900f8a5d9456b3beb048732a19d3b779db9
skull-crusher-aze <87946333+skull-crusher-aze@users.noreply.github.com>
2024-03-02 01:06:00 +0400

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int say = 1;
+        say = 13;
+        byte sayi = 123;
+        double dubli = 12.3;
+        char karakteri = 's';
+        boolean eheh = true;
+        System.out.println(say);
+        System.out.println(sayi);
+        System.out.println(dubli);
+        System.out.println(karakteri);
+        System.out.println(eheh);
+    }
+}
