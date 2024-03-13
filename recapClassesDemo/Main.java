13a74766459dddb010bc789be1cf0ce633a9cb13
skull-crusher-aze <87946333+skull-crusher-aze@users.noreply.github.com>
2024-03-13 18:50:21 +0400

    Add files via upload

---
+++ recapClassesDemo/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        fourCalculcations calc = new fourCalculcations();
+        int res = calc.adding(3,4);
+        System.out.println(res);
+    }
+}
---
+++ recapClassesDemo/fourCalculcations.java
@@ -0,0 +1,14 @@
+public class fourCalculcations {
+    public int adding(int a, int b){
+        return a + b;
+    }
+    public int extract(int a, int b){
+        return a - b;
+    }
+    public int multiplication(int a, int b){
+        return a * b;
+    }
+    public int divide(int a, int b){
+        return a / b;
+    }
+}
